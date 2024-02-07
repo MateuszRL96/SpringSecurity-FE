e6b906f3301f61980e983c5d7b61949ca582c113
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-02-07 20:40:21 +0100

    czesc druga

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,17 +2,10 @@ import { NgModule } from '@angular/core';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from '../shared/shared.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [
-    SharedModule,
-    AuthRoutingModule,
-    MatFormFieldModule,
-    MatInputModule,
-  ],
+  imports: [SharedModule, AuthRoutingModule],
 })
 export class AuthModule {}
---
+++ src/app/modules/core/models/forms.model.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { FormControl } from '@angular/forms';
+
+export interface LoginForm {
+  login: FormControl<string>;
+  password: FormControl<string>;
+}
+
+export interface RegisterForm extends LoginForm {
+  email: FormControl<string>;
+  repeatedPassword: FormControl<string>;
+}
