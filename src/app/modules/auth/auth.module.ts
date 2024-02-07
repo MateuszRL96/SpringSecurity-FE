b733e75f632436ad5087c712e7f0ae5e07f5cbc5
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-02-07 15:36:26 +0100

    czesc pierwsza

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { LoginComponent } from './components/login/login.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+    SharedModule,
+    AuthRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
