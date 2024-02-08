98df218df9ef11bf706178ea5685d6d340ac0051
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-02-08 13:03:13 +0100

    czesc czwarta

---
+++ src/app/modules/auth/store/auth.effecs.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@angular/core';
+import { Actions } from '@ngrx/effects';
+import { AuthService } from '../../core/services/auth.service';
+
+@Injectable()
+export class AuthEffects {
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+  ) {}
+}
