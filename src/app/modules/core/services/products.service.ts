7d4c99eb5d8c07ecd2289c0188bb6530a54c9f12
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-02-24 13:17:31 +0100

    dodanie strony dla osobnego produktu, oraz wyswietlenie informacji w konsoli

--- src/app/modules/core/services/products.service.ts
+++ src/app/modules/core/services/products.service.ts
@@ -3,7 +3,11 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { GetProductsResponse, PrimitiveProduct } from '../models/product.model';
+import {
+  GetProductsResponse,
+  PrimitiveProduct,
+  Product,
+} from '../models/product.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +16,16 @@ export class ProductsService {
   apiUrl = `${environment.apiUrl}/product`;
 
   constructor(private http: HttpClient) {}
+
+  getProduct(name: string, date: string): Observable<Product> {
+    const params = new HttpParams()
+      .append('name_like', name)
+      .append('data', date);
+
+    return this.http.get<Product>(`${this.apiUrl}`, {
+      params,
+    });
+  }
 
   getProducts(
     pageIndex = 1,
