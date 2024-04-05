5258d7e95a66e578de71b738f0a942a21a3ec4ae
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-04-05 18:30:01 +0200

    jhihiohjoip;jop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
